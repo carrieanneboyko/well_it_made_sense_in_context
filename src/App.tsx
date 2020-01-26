19553ca2d9c3243673fc1bcd336d0c89d2232bac
brianboyko <brian.boyko@gmail.com>
2020-01-26 20:45:05 +0000

    Added react router so I can switch to different parts of the project

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+import SimpleCounter from "./components/SImpleCounter/SimpleCounter";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/simple-counter">Simple Counter using Hooks</Link>
+              </li>
+              {/* <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li> */}
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/simple-counter">
+              <SimpleCounter />
+            </Route>
+            {/* <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route> */}
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SImpleCounter/SimpleCounter.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const SimpleCounter: React.FC<{}> = () => {
+  const [count, setCount] = useState<number>(0);
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+  const setToExactlySeven = () => {
+    setCount(7);
+  };
+  return (
+    <div className="simple-counter">
+      <h1>{count}</h1>
+      <div>
+        <button onClick={increment}>Increment</button>
+      </div>
+      <div>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={setToExactlySeven}>Set count to exactly 7</button>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleCounter;
